272039b62923a796fa1876abb38bf95484adf127
Ankit Billa <46354723+MeteoRex11@users.noreply.github.com>
2021-03-01 23:38:51 +0530

    Update Mobile_model.py
    Single Channel Decoder output for now

--- MobileNetV2/Mobile_model.py
+++ MobileNetV2/Mobile_model.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from icecream import ic
+import numpy as np
 
 class UpSample(nn.Sequential):
     def __init__(self, skip_input, output_features):
@@ -20,16 +22,16 @@ class Decoder(nn.Module):
         features = int(num_features * decoder_width)
 
         self.conv2 = nn.Conv2d(num_features, features, kernel_size=1, stride=1, padding=1)
-        
         self.up0 = UpSample(skip_input=features//1 + 320, output_features=features//2)
         self.up1 = UpSample(skip_input=features//2 + 160, output_features=features//2)
         self.up2 = UpSample(skip_input=features//2 + 64, output_features=features//4)
         self.up3 = UpSample(skip_input=features//4 + 32, output_features=features//8)
-        self.up4 = UpSample(skip_input=features//8 +  24, output_features=features//8)
-        self.up5 = UpSample(skip_input=features//8 +  16, output_features=features//16)
-
+        self.up4 = UpSample(skip_input=features//8 + 24, output_features=features//8)
+        self.up5 = UpSample(skip_input=features//8 + 16, output_features=features//16)
         self.conv3 = nn.Conv2d(features//16, 1, kernel_size=3, stride=1, padding=1)
 
+
+    
     def forward(self, features):
         x_block0, x_block1, x_block2, x_block3, x_block4,x_block5,x_block6 = features[2], features[4], features[6], features[9], features[15],features[18],features[19]
         x_d0 = self.conv2(x_block6)
@@ -39,7 +41,8 @@ class Decoder(nn.Module):
         x_d4 = self.up3(x_d3, x_block2)
         x_d5 = self.up4(x_d4, x_block1)
         x_d6 = self.up5(x_d5, x_block0)
-        return self.conv3(x_d6)
+        temp = self.conv3(x_d6)
+        return temp
 
 class Encoder(nn.Module):
     def __init__(self):
@@ -48,8 +51,13 @@ class Encoder(nn.Module):
         self.original_model = models.mobilenet_v2( pretrained=True )
 
     def forward(self, x):
+        from icecream import ic
+        print("-----------------------------------------------------")
         features = [x]
-        for k, v in self.original_model.features._modules.items(): features.append( v(features[-1]) )
+
+        for k, v in self.original_model.features._modules.items():
+            features.append( v(features[-1]))
+        
         return features
 
 class Model(nn.Module):
